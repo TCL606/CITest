0e13b9bc9cf7e97dbc47b44a92cc3be92898c8f7
TCL <84725343+TCL606@users.noreply.github.com>
2022-06-02 22:22:30 +0800

    Update Program.cs

--- CSharp_Calculation/Program.cs
+++ CSharp_Calculation/Program.cs
@@ -5,7 +5,7 @@ namespace CSharp_Calculation
     public class Program{
         static void Main(string[] args)
         {
- int num1 = 2, num2 = 1;
+            int num1 = 2, num2 = 1;
             Operation<int> op = new Add<int>();
             Console.WriteLine(op.Operate(num1, num2));
             op = new Sub<int>();
